c28e46e7ff0ad8df8651e64ee565317522affdc1
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2021-02-13 09:43:13 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,24 +2,40 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
-namespace Ui {
-class MainWindow;
-}
+#include "counter.h"
+#include <QThread>
+#include "windows.h"
+#include "exmple.h"
+#include "QtSerialPort/QtSerialPort"
+#include "QSqlDatabase"
+#include <QMessageBox>
+#include "QSqlDriver"
+#include "QSqlQuery"
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+public slots:
+    void rep(QString j);
+signals:
 
 private slots:
-    void on_pushButton_clicked();
+    void on_poggyButt_clicked();
+
+
 
 private:
     Ui::MainWindow *ui;
+    QThread thred_1;
+    QThread thred_2;
+    exmple ex_1;
+    exmple ex_2;
 };
-
 #endif // MAINWINDOW_H
